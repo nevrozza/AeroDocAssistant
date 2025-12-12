a1dd51da44e4ea86b451a69cf0646ebcc5d2cad7
TheMrKan <mrkan2017@gmail.com>
2025-12-13 01:40:58 +0300

    структура бэкенда

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,3 @@
-from fastapi import FastAPI
 
-PROJECT_NAME = "AeroDocAssistant"
-
-app = FastAPI(title=f"{PROJECT_NAME}")
-
-
-@app.get("/",
-         summary="Статус API",
-         description="Проверка работы API",
-         response_description=f'Возвращает {PROJECT_NAME} работает!"'
-         )
-async def root():
-    return {"message": f"{PROJECT_NAME} работает!"}
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True)
+if __name__ == '__main__':
+    pass

a8365feff2f9a29eade3185435fb59087d38c2f3
Artyom Matashkov <aimatashkov@mai.education>
2025-12-13 05:41:29 +0300

    - Create IconButton - Add colors from css to TS - Add ReactIcons lib

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from "./app.tsx";
 import './styles/index.css'
 import './styles/theme.css'
+import './styles/common.css'
 
 
 const root = document.getElementById('root') as HTMLElement;
--- frontend/src/pages/dummy-page/dummy-page.tsx
+++ frontend/src/pages/dummy-page/dummy-page.tsx
@@ -1,12 +1,18 @@
 import "./dummy-page.css"
 import type {FC} from "react";
+import {LuIceCreamCone} from "react-icons/lu"
+import {IconButton, colors} from "../../widgets";
+
 
 interface DummyPageProps {
     count: number;
 }
 
 const DummyPage: FC<DummyPageProps> = ({count}) => {
-    return <h1>Dummy+{count}</h1>;
+    return <div>
+        <h1>Dummy+{count}</h1>
+        <IconButton icon={LuIceCreamCone} iconColor={colors.onBackground}/>
+    </div>;
 };
 
 export default DummyPage;

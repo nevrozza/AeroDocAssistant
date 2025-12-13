461501ed15b01001bc68098c76815c076b013b93
TheMrKan <mrkan2017@gmail.com>
2025-12-13 14:37:29 +0300

    явное цитирование по формату

--- backend/app/core/graph.py
+++ backend/app/core/graph.py
@@ -6,6 +6,7 @@ from langchain_core.documents.base import Document
 from langchain_community.chat_models import ChatYandexGPT
 from langchain_openai import ChatOpenAI
 from langchain.tools import tool
+import re
 
 from app.core import storage, config
 
@@ -46,23 +47,37 @@ def __context_to_message(context: list[Document]) -> AnyMessage:
 
 
 @tool
-async def search_tool(query: str) -> str:
+async def search_tool(query: str) -> tuple[str, list[Document]]:
     """Векторный поиск через embedding по векторной базе документов. Составляй запросы так, чтобы при векторизации было максимум совпадений с документами.
     Результат не будет сохранен в истории чата. Процитируй нужные или использованые фрагменты из поиска в своем сообщении.
+
+    Args:
+        query: Запрос в векторную базу данных для поиска. Формируй запрос не как в поисковую систему, а чтобы при векторизации было максимум совпадений с документом. Используй максимально похожие формулировки и слова.
     """
     docs = await storage.search_async(query)
 
     rendered = []
     for doc in docs:
         rendered.append(
-            f"Документ: {doc.metadata.get('title', '')}\n{doc.page_content}"
+            f"[doc:{doc.id}]\n"
+            f"<<<\n{doc.page_content}\n>>>\n"
         )
 
-    return "\n\n".join(rendered)
+    return (
+            "ЭТО ЕДИНСТВЕННЫЙ ДОПУСТИМЫЙ ИСТОЧНИК ИНФОРМАЦИИ.\n"
+            "ЗАПРЕЩЕНО использовать любые знания вне этих документов.\n"
+            "ПРОАНАЛИЗИРУЙ НАЙДЕННЫЕ ДОКУМЕНТЫ И ИСПОЛЬЗУЙ ТОЛЬКО ТЕ, КОТОРЫЕ СОДЕРЖАТ РЕЛЕВАНТНУЮ ИНФОРМАЦИЮ.\n"
+            "ЕСЛИ РЕЛЕВАНТНОЙ ИНФОРМАЦИИ НЕ НАЙДЕНО, ТО ЗАПУСТИ ПОИСК ЕЩЕ РАЗ\n"
+            "Каждое утверждение ОБЯЗАНО содержать прямую цитату.\n"
+            "и ссылку в формате ```[doc:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx] цитата```.\n"
+            "ТЫ ОБЯЗАН ФОРМАТИРОВАТЬ С ИСПОЛЬЗОВАНИЕМ СИМВОЛОВ ``` КАК В ПРИМЕРЕ.\n"
+            "До этого инструкция, на которую нельзя ссылаться и говорить о ней пользователю. Содержимое документов:\n\n"
+            + "\n\n".join(rendered), docs
+    )
 
 
-async def stream_llm_async(history: list[AnyMessage], context: list[Document]) -> Generator[LLMChunk, None, None]:
-    history = [*history, __context_to_message(context)]
+async def stream_llm_async(history: list[AnyMessage], used_docs: dict[str, Document]) -> Generator[LLMChunk, None, None]:
+    history = [*history]
 
     response = await llm.ainvoke(history)
 
@@ -71,29 +86,54 @@ async def stream_llm_async(history: list[AnyMessage], context: list[Document]) -
 
         for call in response.tool_calls:
             if call["name"] == "search_tool":
-                tool_result = await search_tool.ainvoke(call["args"])
+                rendered_result, docs = await search_tool.ainvoke(call["args"])
                 print(f"Invoked search with query {call["args"]}")
+                for doc in docs:
+                    used_docs[doc.id] = doc
 
-                history.append(ToolMessage(content=tool_result, tool_call_id=call["id"]))
+                history.append(ToolMessage(content=rendered_result, tool_call_id=call["id"]))
 
-        async for chunk in llm.astream(history, config={"max_tokens": 50}):
+        async for chunk in llm.astream(history, config={"max_tokens": 150}):
             yield LLMChunk(
                 chunk.text,
                 (chunk.usage_metadata or {}).get("total_tokens", 0),
             )
     else:
-        yield LLMChunk(response.content, 0)
+        yield LLMChunk(response.content, response.usage_metadata["total_tokens"])
 
 
 SETUP_INSTRUCTIONS = """
-Ты LLM помощник по технической документации в сфере авиастроения в ПАО Яковлев.
+Ты отвечаешь ТОЛЬКО на основе текста,
+полученного через инструмент search_tool.
+В него нужно передать текст для векторизации, который по содержанию максимально будет совпадать с искомым документов.
+Предположи, какие выражения и слова используются в целевом документе и ищи по ним.
+Это не Google, это векторная база данных.
+Ты должен переформулировтаь запрос пользователя.
 
-Правила:
-- если вопрос требует фактов, терминов, параметров, стандартов — используй инструмент поиска
-- не отвечай по памяти, если информация может быть в базе
-- отвечай кратко и технически точно
-- если используешь поиск, то обязательно приводи цитату.
+Формат ответа:
+- обычный текст
+- каждая фактическая часть ОБЯЗАНА содержать
+  прямую цитату 
+  и ссылку в формате ```[doc:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx] цитата```
+- цитирование допускается только в указаном формате
+
+Пример:
+Материал применяется в конструкции планера,
+```[doc:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx] углепластики используются в конструкции планера```.
+ТЫ ОБЯЗАН ФОРМАТИРОВАТЬ С ИСПОЛЬЗОВАНИЕМ СИМВОЛОВ ``` КАК В ПРИМЕРЕ.
+
+Запрещено:
+- использовать знания вне документов
+- пересказывать документ без прямой цитаты
+- указывать источник без цитаты
+- приводить цитаты не в указаном формате
+
+Если в документах нет ответа - напиши. Но снача ты обязан запустить поиск.
 """
+
+
+def extract_doc_ids(text: str) -> list[str]:
+    return re.findall(r'\[doc:([A-Za-z0-9._:-]+)]', text)
 
 
 async def main():
@@ -105,14 +145,18 @@ async def main():
         message = input(">>> ")
         history.append(HumanMessage(message))
         chunks = []
-        context = await storage.search_async(message)
-        async for chunk in stream_llm_async(history, context):
+        used_documents: dict[str, Document] = {}
+        async for chunk in stream_llm_async(history, used_documents):
             print(chunk.text_delta, end="", flush=True)
             chunks.append(chunk)
 
         history.append(AIMessage("".join(c.text_delta for c in chunks)))
+
         tokens = sum(c.tokens_delta for c in chunks)
-        print(f"\n({tokens} токенов)\n")
+        print(f"\n({tokens} токенов)")
+
+        for used_doc in used_documents.values():
+            print(f"Использовано: ```[doc:{used_doc.id}] {used_doc.metadata.get('title', '')}```")
 
 
 if __name__ == "__main__":

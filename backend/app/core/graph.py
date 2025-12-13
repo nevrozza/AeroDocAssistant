820ece10939e638545c854286aa8a5206b708245
TheMrKan <mrkan2017@gmail.com>
2025-12-13 03:31:18 +0300

    стриминг через Yandex Cloud LLM

---
+++ backend/app/core/graph.py
@@ -0,0 +1,71 @@
+import asyncio
+import os
+from typing import Generator
+from dataclasses import dataclass
+from yandex_cloud_ml_sdk import AsyncYCloudML
+from yandex_cloud_ml_sdk._models.completions.model import AsyncGPTModel
+from langchain.messages import (HumanMessage, AIMessage, SystemMessage, AnyMessage)
+
+API_KEY = os.getenv("YC_API_KEY")
+FOLDER = os.getenv("YC_FOLDER")
+
+
+llm: AsyncGPTModel
+
+
+def setup():
+    global llm
+    sdk = AsyncYCloudML(
+        auth=API_KEY,
+        folder_id=FOLDER,
+    )
+    model = sdk.models.completions("yandexgpt-lite")
+    llm = model.configure(max_tokens=50)
+
+
+@dataclass
+class LLMChunk:
+    text_delta: str
+    tokens_delta: int
+
+
+ROLES_MAP = {
+    "human": "user",
+    "ai": "assistant",
+    "system": "system"
+}
+
+
+async def stream_llm_async(history: list[AnyMessage]) -> Generator[LLMChunk, None, None]:
+    prev_text = ""
+    prev_tokens = 0
+    async for chunk in llm.run_stream([{"role": ROLES_MAP[m.type], "text": m.text} for m in history]):
+        text_delta = chunk.alternatives[0].text.lstrip(prev_text)
+        prev_text = chunk.alternatives[0].text
+
+        tokens_delta = chunk.usage.total_tokens - prev_tokens
+        prev_tokens = chunk.usage.total_tokens
+        yield LLMChunk(text_delta, tokens_delta)
+
+
+async def main():
+    setup()
+
+    history = [SystemMessage("Ты LLM помощник по технической документации в сфере авиастроения в ПАО Яковлев. "
+                             "Твоя задача - максимально экспертно отвечать сотрудникам на поставленные вопросы. "
+                             "Без воды, только факты и реальная помощь.")]
+    while True:
+        message = input(">>> ")
+        history.append(HumanMessage(message))
+        chunks = []
+        async for chunk in stream_llm_async(history):
+            print(chunk.text_delta, end="", flush=True)
+            chunks.append(chunk)
+
+        history.append(AIMessage("".join(c.text_delta for c in chunks)))
+        tokens = sum(c.tokens_delta for c in chunks)
+        print(f"\n({tokens} токенов)\n")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

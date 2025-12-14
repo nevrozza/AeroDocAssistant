003a9c502bc68fd67cdd58c9c6f0f4a8ff409a02
TheMrKan <mrkan2017@gmail.com>
2025-12-15 01:17:25 +0300

    схемы документов

--- backend/app/api/chat_router.py
+++ backend/app/api/chat_router.py
@@ -8,10 +8,9 @@ from uuid import uuid4
 router = APIRouter(prefix="/chat")
 
 
-class MessageSchema(BaseModel):
-    role: Literal["user", "assistant"]
-    text: str
-    used_fragments: list[UUID4]
+class DocumentSchema(BaseModel):
+    document_id: UUID4
+    title: str
 
 
 class FragmentSchema(BaseModel):
@@ -19,6 +18,12 @@ class FragmentSchema(BaseModel):
     text: str
     source: UUID4
     source_page: int
+
+
+class MessageSchema(BaseModel):
+    role: Literal["user", "assistant"]
+    text: str
+    used_fragments: list[UUID4]
 
 
 class ChatMetadataSchema(BaseModel):
@@ -29,6 +34,7 @@ class ChatMetadataSchema(BaseModel):
 class ChatContentSchema(ChatMetadataSchema):
     messages: list[MessageSchema]
     used_fragments: list[FragmentSchema]
+    used_documents: list[DocumentSchema]
 
 
 @router.get("/", response_model=list[ChatMetadataSchema])
@@ -49,8 +55,12 @@ async def create_chat() -> ChatMetadataSchema:
 
 @router.get("/{chat_id}/", response_model=ChatContentSchema)
 async def get_chat(chat_id: UUID4) -> ChatContentSchema:
+    documents = [
+        DocumentSchema(document_id=uuid4(), title="Заголовок документа")
+    ]
     fragments = [
-        FragmentSchema(fragment_id=uuid4(), text="Текст первого фрагмента из которого будет взята цитата")
+        FragmentSchema(fragment_id=uuid4(), text="Текст первого фрагмента из которого будет взята цитата",
+                       source=documents[0].document_id, source_page=2)
     ]
     return ChatContentSchema(
         chat_id=chat_id,
@@ -59,5 +69,6 @@ async def get_chat(chat_id: UUID4) -> ChatContentSchema:
             MessageSchema(role="user", text="Первый вопрос?", used_fragments=[]),
             MessageSchema(role="assistant", text=f"Ответ LLM на первый вопрос {{{{{{[frag:{fragments[0].fragment_id}] будет взята цитата}}}}}}", used_fragments=[fragments[0].fragment_id])
         ],
-        used_fragments=fragments
+        used_fragments=fragments,
+        used_documents=documents
     )

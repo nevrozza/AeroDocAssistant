6202cf18cc401655d7a9b6e7cc9ece6851de7e7d
MAOkunev <maokunev@mai.education>
2025-12-15 00:27:24 +0300

    Added select-button + graph test

---
+++ frontend/src/pages/graph-page/graph-page.tsx
@@ -0,0 +1,20 @@
+import { GraphCanvas } from 'reagraph';
+import "./graph-page.css"
+
+
+interface GraphPageProps{
+    edges: Array<{id: string, target: string, source: string, label: string}>
+}
+
+const GraphPage = ({edges}: GraphPageProps) =>{
+    const nodes = [
+        {id: "1", label: "first"},
+        {id: "2", label: "second"},
+        {id: "3", label: "third"}
+    ]
+    
+    return <div className='container'><GraphCanvas edges={edges} nodes={nodes}></GraphCanvas></div>
+
+}
+
+export default GraphPage
--- frontend/src/widgets/index.tsx
+++ frontend/src/widgets/index.tsx
@@ -1,7 +1,8 @@
 import IconButton from "./icon-button/icon-button.tsx"
 import OutlinedButton from "./outlined-button/outlined-button.tsx"
 import TextField from "./textfield/textfield.tsx"
+import SelectButton from "./select-button/select-button.tsx"
 import {colors, type ColorsVarKey} from "./utils/colors.ts"
 import {type Icon} from "./utils/icon.ts"
 
-export {IconButton, OutlinedButton, TextField, colors, type ColorsVarKey, type Icon}
+export {IconButton, OutlinedButton, TextField, SelectButton, colors, type ColorsVarKey, type Icon}

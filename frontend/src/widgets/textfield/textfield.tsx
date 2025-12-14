bbb21733bf02f1d2e814f5d4b12beec48e09b5f7
Artyom Matashkov <aimatashkov@mai.education>
2025-12-14 08:06:21 +0300

    - Return extra onChange - Add trailing icon hide animation

--- frontend/src/widgets/textfield/textfield.tsx
+++ frontend/src/widgets/textfield/textfield.tsx
@@ -1,4 +1,4 @@
-import type { Icon } from "../utils/icon";
+import type {Icon} from "../utils/icon";
 import "./textfield.css"
 import {colors, IconButton} from "../../widgets"
 import {type FC, type RefObject, useEffect, useState} from "react";
@@ -6,7 +6,9 @@ import * as React from "react";
 
 interface TextFieldProps {
     ref: RefObject<HTMLTextAreaElement | null>;
+    onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
     trailingIcon?: Icon;
+    trailingIconHidable?: boolean;
     onTrailingIconClick?: () => void;
     placeholder?: string;
     minLines?: number;
@@ -14,11 +16,13 @@ interface TextFieldProps {
 }
 
 const TextField: FC<TextFieldProps> = (props: TextFieldProps) => {
-
     const minBorderRadius = 20; // минимальное скругление при максимальном количестве строк
     const maxBorderRadius = 30; // максимальное скругление при одной строке
     const [currentLines, setCurrentLines] = useState(props.minLines || 1);
     const [borderRadius, setBorderRadius] = useState(maxBorderRadius);
+
+    const trailingIconHidable = props.trailingIconHidable || true
+    const [trailingIconVisible, setTrailingIconVisible] = useState(true);
 
 
     useEffect(() => {
@@ -41,11 +45,14 @@ const TextField: FC<TextFieldProps> = (props: TextFieldProps) => {
     }, [currentLines, props.maxLines]);
 
     const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        props.onChange?.(e);
+        setTrailingIconVisible(e.target.value === '' || !trailingIconHidable);
+
         // Auto-resize
         const textarea = e.target;
         textarea.style.height = 'auto';
 
-        const lineHeight = 16*1.5;
+        const lineHeight = 16 * 1.5;
         const minHeight = (props.minLines || 1) * lineHeight;
         const maxHeight = (props.maxLines || 5) * lineHeight;
 
@@ -66,12 +73,22 @@ const TextField: FC<TextFieldProps> = (props: TextFieldProps) => {
                 onChange={handleChange}
                 rows={props.minLines || 1}
                 style={{
-                    minHeight: `${(props.minLines || 1) * 24}px`,
-                    maxHeight: `${(props.maxLines || 5) * 24}px`,
+                    minHeight: `${(props.minLines || 1) * 16 * 1.5}px`,
+                    maxHeight: `${(props.maxLines || 5) * 16 * 1.5}px`,
+                    paddingLeft: 30,
+                    paddingRight: trailingIconHidable ? 30 : 0,
                 }}
             />
             {props.trailingIcon && (
-                <div className="trailing-icon">
+                <div className="trailing-icon"
+                     style={{
+                         transition: 'all 0.3s ease',
+                         opacity: trailingIconVisible ? 1 : 0,
+                         transform: `translateX(${trailingIconVisible ? 0 : '10px'})`,
+                         pointerEvents: trailingIconVisible ? 'auto' : 'none',
+                         position: 'absolute',
+                         right: 0, // абсолют в правой части
+                     }}>
                     <IconButton
                         onClick={props.onTrailingIconClick}
                         icon={props.trailingIcon}

b6867551587599e651439c8dadeb4bd2bb49b57f
MrKan <mrkan2017@gmail.com>
2025-12-16 11:59:02 +0300

    скачивание PDF документа

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.api.chat_rest import router as chat_rest_router
 from app.api.chat_ws import router as chat_ws_router
+from app.api.documents_rest import router as document_rest_router
 from app.core.container import Container
 
 
@@ -11,4 +12,5 @@ app = FastAPI()
 
 app.include_router(chat_rest_router)
 app.include_router(chat_ws_router)
+app.include_router(document_rest_router)
 
